c6fc29013323e268cafd4c6a326ac567a804c129
TanushLichade <143272316+TanushLichade@users.noreply.github.com>
2025-09-18 12:11:16 +0530

    Update upload_data.py

--- upload_data.py
+++ upload_data.py
@@ -3,7 +3,7 @@ import pandas as pd
 import json
 
 #url
-uri = "mongodb+srv://tanushlichade:Tanush@cluster0.jvum79r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+uri = "..."
 
 #create a new client and connectt to server
 client = MongoClient(uri)
